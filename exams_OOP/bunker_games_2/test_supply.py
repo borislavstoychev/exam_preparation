bab0108f8ca9b79d1127ca201199cdf6ec441e4a
borislavstoychev <stoy4ew@gmail.com>
2021-04-08 22:44:22 +0300

    tests class attribut child class

--- exams_OOP/bunker_games_2/test_supply.py
+++ exams_OOP/bunker_games_2/test_supply.py
@@ -6,7 +6,7 @@ from project.supply.food_supply import FoodSupply
 from project.supply.water_supply import WaterSupply
 
 
-class TestSupply(TestCase):
+class TestSupplyMedicine(TestCase):
     def setUp(self) -> None:
         self.painkiller = Painkiller()
         self.water_supply = WaterSupply()
@@ -20,14 +20,15 @@ class TestSupply(TestCase):
         self.assertEqual(40, self.water_supply.needs_increase)
 
     def test_needs_increase_raise_error(self):
-        self.painkiller.health_increase = - 5
-        # self.salve.health_increase = -20
-        self.food_supply.needs_increase = -10
-        self.water_supply.needs_increase = -10
-        self.assertEqual(-5, self.painkiller.health_increase)
-        self.assertEqual(-20, self.salve.health_increase)
-        self.assertEqual(-10, self.food_supply.needs_increase)
-        self.assertEqual(-10, self.water_supply.needs_increase)
+        with self.assertRaises(ValueError):
+            self.painkiller.health_increase = - 5
+            self.salve.health_increase = -20
+            self.food_supply.needs_increase = -10
+            self.water_supply.needs_increase = -10
+        # self.assertEqual(-5, self.painkiller.health_increase)
+        # self.assertEqual(-20, self.salve.health_increase)
+        # self.assertEqual(-10, self.food_supply.needs_increase)
+        # self.assertEqual(-10, self.water_supply.needs_increase)
     #
     # def test_apply(self):
     #     self.fail()
